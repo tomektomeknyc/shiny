9704dfe3ffba5a1986db7aa4cb03bf4336a52b0d
Tom Osuchowski <tomektomeknyc@yahoo.com>
2022-01-14 22:24:40 +0100

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+covid <- read.csv2("COVID.csv")
+# A fluid  layout
+fluidPage(
+
+  # Title
+  titlePanel("COVID infections per country in January 2022"),
+
+  # Create a row with a sidebar
+  sidebarLayout(
+
+
+    # Sidebar layout containing input controls
+    sidebarPanel(p( a("User Information", style="color:green",href="https://rpubs.com/tomektomeknyc/855548")),
+                 selectInput("Country", "Country:",
+                             choices=colnames(covid[-1])),
+                 hr(),
+                 helpText("Covid infections in the first week of 2022")
+    ),
+
+    # Barplot
+    mainPanel(
+      plotOutput("infections")
+    )
+
+  )
+)
+
